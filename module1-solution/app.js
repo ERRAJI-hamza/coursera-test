4758d60102fc64eb8679feb375bd9f4407457b98
hamza erraji <hamzaerr@gmail.com>
2023-03-07 02:13:30 +0100

    'app.js'

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -6,6 +6,20 @@
     
     M1Controller.$inject = ['$scope'];
     function M1Controller($scope) {
-      
+       $scope.message='';
+       $scope.dishes='';
+       $scope.checker=function(){
+        var arr = $scope.dishes.split(",");
+        if($scope.dishes == ''){
+            $scope.message='entre your dishes!'
+        }
+        else if(arr.length <= 3){
+            $scope.message="Enjoy!";
+        }
+        else{
+            $scope.message="Too much!";
+        }
+       }
+
     }
 })();
